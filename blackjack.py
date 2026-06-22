1d86d79ff49b604c857ce2b06c0df215926a5f02
eric <eric@Laptop-Eri.local>
2026-06-23 00:27:13 +0200

    more additions on the dealer logic, playable but not very pretty

--- blackjack.py
+++ blackjack.py
@@ -1,12 +1,15 @@
 import sys
+from time import sleep
 from random import shuffle
 from random import choice
 
 
 #helper methods, sorted alphabetically
-def calculate_score(hand, score):
+def calculate_score(hand):
 
 	#calculating the score of a hand provided via input, returns the score of the hand
+	
+	score = 0
 	
 	#this step is pretty ineffective
 	for cards in hand:
@@ -79,18 +82,19 @@ def init_game(deck, player_hand, dealer_hand):
 	dealer_hand.append("UNKNOWN")
 	
 	#calculates scores
-	player_score = calculate_score(player_hand, 0)
-	dealer_score = calculate_score(dealer_hand, 0)	
+	player_score = calculate_score(player_hand)
+	dealer_score = calculate_score(dealer_hand)
+	dealer_score = str(dealer_score) + " + ?" 	
 	
 	#print hands + scores after game start
-	print("Player: " + player_hand[0] + ", " + player_hand[1] + " -> Score: " + str(player_score))
-	print("Dealer: " + dealer_hand[0] + ", " + dealer_hand[1] + " -> Score: " + str(dealer_score) + " + ?")
-
+	print_scores(player_hand, player_score, dealer_hand, dealer_score)
 	return deck, player_hand, player_score, dealer_hand, dealer_score
 	
 
-def print_scores(player_hand, dealer_hand):
-	#TODO!!!		
+def print_scores(player_hand, player_score, dealer_hand, dealer_score):
+	print("\n" + "Player: " + ", ".join(player_hand) + " -> Score: " + str(player_score))
+	print("Dealer: " + ", ".join(dealer_hand) + " -> Score: " + str(dealer_score) + "\n")
+	return		
 	
 def main():
 	#init deck
@@ -111,25 +115,23 @@ def main():
 
 	while players_turn:
 		#prompt user if an additional card is desired
-		if input("Another Card? (y/n): ").lower() == "y":
-			
-			player_score = 0		
+		if input("Another Card? (y/n): ").lower() == "y":		
 	
 			#draw new card, add it to player hand, calc player_score
 			deck, card = deal_card(deck)
 			player_hand.append(card)
-			player_score = calculate_score(player_hand, player_score)
+			player_score = calculate_score(player_hand)
 			
 			#if score exceeds 21
 			if(player_score > 21):
-				print("Game Over, Score: " + f"{player_score}")
+				print("Game Over, your score is: " + f"{player_score}" + "\nTherefore you lose.")
 				sys.exit()
 			#if score is lower than 21
 			elif(player_score < 21):
-				print("Player: " + ", ".join(player_hand) + " -> Score: " + str(player_score))
-				print("Dealer: " + ", ".join(dealer_hand) + " -> Score: " + str(dealer_score))
+				print_scores(player_hand, player_score, dealer_hand, dealer_score)
 			#score is 21
 			else:
+				print_scores(player_hand, player_score, dealer_hand, dealer_score)
 				print("BLACK JACK, you won!!!")
 				sys.exit()
 		else:
@@ -140,24 +142,39 @@ def main():
 	dealer_hand.remove("UNKNOWN")
 	deck, card = deal_card(deck)
 	dealer_hand.append(card)
+	dealer_score = calculate_score(dealer_hand)
+	
+	print_scores(player_hand, player_score, dealer_hand, dealer_score)
+	sleep(3)
 
 	dealers_turn = True
 	while dealers_turn:
-		dealer_score = 0
 		
-		dealer_score = calculate_score(dealer_hand, dealer_score)
+		sleep(3)
+		
 		if dealer_score < 17:
 			deck, card = deal_card(deck)
 			dealer_hand.append(card)
 			
+			dealer_score = calculate_score(dealer_hand)
 			#print the scores
-			print("Player: " + ", ".join(player_hand) + " -> Score: " + str(player_score))
-			print("Dealer: " + ", ".join(dealer_hand) + " -> Score: " + str(dealer_score))
+			print_scores(player_hand, player_score, dealer_hand, dealer_score)
 		elif 17 <= dealer_score <= 21:
+			dealer_score = calculate_score(dealer_hand)
+			print_scores(player_hand, player_score, dealer_hand, dealer_score)
 			dealers_turn = False
 		else:
-			print("Dealer went over 21, you won!!!")
+			print("Dealer went over 21, you won!!! Your Score: " + f"{player_score}")
 			sys.exit()	
-	#print(deck)
+	
+	#game end, this scenario happens, when dealers score is between 17 and 21 and player passed on his turn
+	if player_score > dealer_score:
+		print("\nYou won. Your Score: " + f"{player_score}, " + "Dealers Score: " + f"{dealer_score}")
+	elif player_score < dealer_score:
+		print("\nDealer won. Your Score: " + f"{player_score}, " + "Dealers Score: " + f"{dealer_score}")
+	else:
+		print("\nDraw! You both scored: " + f"{player_score}") 
+	
+	print(deck)
 
 main()
